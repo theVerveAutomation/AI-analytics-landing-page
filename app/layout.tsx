53616aef417e9cc6ba725fa4f78f5a4c7324e8ae
klperera <klperera660@gmail.com>
2025-12-30 09:42:31 +0530

    feat: add custom ThemeProvider component for improved theme management

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "next-themes";
+import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "sonner";
 
 const geistSans = Geist({
